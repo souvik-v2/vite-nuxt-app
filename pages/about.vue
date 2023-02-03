a798ba247c5366730a20bd0944ec51b620de4b37
Souvik Patra <85334737+souvik-v2@users.noreply.github.com>
2023-02-03 14:26:42 +0530

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h3 class="text-2xl">About Us</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam
+      temporibus deleniti blanditiis sed recusandae necessitatibus cumque
+      repellendus nemo, quia delectus minima, saepe rem consequatur distinctio,
+      facere magni atque debitis.
+    </p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="vite-muxt-app">
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <div class="main">
+      <h2>Welsome nuxt SSR learning!!</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
